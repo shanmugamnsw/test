d377ef4395a9ea9627fb454390093ec24ef8c9c7
shanmugamnsw <65762774+shanmugamnsw@users.noreply.github.com>
2023-02-26 15:30:23 +0530

    Create deploy.groovy

---
+++ deploy.groovy
@@ -0,0 +1,36 @@
+import com.cloudbees.groovy.cps.NonCPS
+import groovy.transform.Field
+
+
+/*----------------------------------------------------------------------------------------------------------------------
+  DEPLOY PIPELINE
+  --------------------------------------------------------------------------------------------------------------------*/
+@NonCPS
+def runDeployPipeline(run) {
+
+     
+     dir('Deployment') {
+	     if(env.inputEnvType.startsWith("STAGE") && (env.inputDeployType == "DEPLOY")){
+             echo  "Deployment is going to run in Staging environments ."      
+	     }
+	     if(env.inputEnvType.startsWith ("<select>") || (env.inputDeployType == "<select>")){
+		     error "Kindly select the environment."
+	     }
+         if(env.inputEnvType.startsWith ("PROD") && (env.inputSrcType == "Mosaic-services")){
+		     echo "Deployment is going to run in Prod-environments in Mosaic-services ." 
+     }
+     }
+     
+             stage('checking'){
+             } 
+        envNamesSplit = env.inputServiceList.tokenize(",");
+        for (i = 0; i < inputServiceList.size();i++) {
+          if (env.inputEnvType == "STAGE"( envNamesSplit[i] )){
+            //if a custom inputEnvnameList is given in input, check if currentEnv name is present in that list.
+            envMatches = true
+          }
+       }
+   
+  }
+
+return this
