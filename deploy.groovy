9ddc9b63687c0656f6c4e8abb7f5a6e62dbf66cd
shanmugamnsw <65762774+shanmugamnsw@users.noreply.github.com>
2023-02-26 22:04:41 +0530

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,37 +1,19 @@
-import com.cloudbees.groovy.cps.NonCPS
-import groovy.transform.Field
 import java.text.SimpleDateFormat
-
+import com.cloudbees.groovy.cps.NonCPS
+@NonCPS
+/*--------------------------------------------------------------------------------------------------------------
+              valid
+---------------------------------------------------------------------------------------------------------------*/
+def validInput(){
+  return (env.inputEnvType != '<select>') && (env.inputEnvSpace != '<select>') && (env.inputServiceList != '')
+}
 
 /*----------------------------------------------------------------------------------------------------------------------
-  DEPLOY PIPELINE
+    PIPELINE Main Function
   --------------------------------------------------------------------------------------------------------------------*/
-@NonCPS
-def runPipeline(props) {
-
-     
-     dir('Deployment') {
-	     if(env.inputEnvType.startsWith("STAGE") && (env.inputDeployType == "DEPLOY")){
-             echo  "Deployment is going to run in Staging environments ."      
-	     }
-	     if(env.inputEnvType.startsWith ("<select>") || (env.inputDeployType == "<select>")){
-		     error "Kindly select the environment."
-	     }
-         if(env.inputEnvType.startsWith ("PROD") && (env.inputSrcType == "Mosaic-services")){
-		     echo "Deployment is going to run in Prod-environments in Mosaic-services ." 
-     }
-     }
-     
-             stage('checking'){
-             } 
-        envNamesSplit = env.inputServiceList.tokenize(",");
-        for (i = 0; i < inputServiceList.size();i++) {
-          if (env.inputEnvType == "STAGE"( envNamesSplit[i] )){
-            //if a custom inputEnvnameList is given in input, check if currentEnv name is present in that list.
-            envMatches = true
-          }
-       }
-   
-  }
+def runPipeline(props){
+	echo "working"
+	
+}
 
 return this;
